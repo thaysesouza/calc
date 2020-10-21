a30088d2690814e24fb044243a0da6b0162c9978
Thayse Souza <thayseenf14@gmail.com>
2020-10-21 13:42:45 -0300

    name_and_age.rb

---
+++ curso_ruby1.rb
@@ -0,0 +1,12 @@
+# saída de dados
+print "Digite seu nome"
+
+#Recebendo uma entrada do teclado
+name = gets.chomp
+
+print "Digite seu sobrenome"
+sobrenome = gets.chomp
+
+#saída utilizando puts
+# use códigos ruby dentro de uma string com #{code}
+puts "Hello #{name} #{sobrenome}!"
---
+++ name_and_age.rb
@@ -0,0 +1,14 @@
+# name and age
+print "Digite seu nome: "
+
+#recebendo uma entrada do teclado
+name = gets.chomp
+
+print "Digite sua idade: "
+
+#recebendo uma entrada do teclado
+age = gets.to_i()
+
+# saída utilizando puts
+# use códigos ruby dentro de uma string com #{code}
+puts "Hello, #{name}, você tem #{age} anos!"
