00c2b26848d978faa5cd9488c32cdbcda2723928
Thayse Souza <thayseenf14@gmail.com>
2020-10-27 11:56:47 -0300

    Exercicio com While - programa para registro de produtos

--- while1.rb
+++ while1.rb
@@ -1,5 +1,5 @@
 # Programa para registro de vendas de produtos de uma padaria.
-# Caso o usuário informe um código inválido (fora da faixa 0 a 4),
+# Caso o usuário informe um código inválido (fora da faixa 1 a 4),
 # deve ser solicitado um novo código (até que seja informado um código válido)
 # códigos: 1.pao 2.bolo 3.brasileira
 # O programa será encerrado quando for informado o código 4.
