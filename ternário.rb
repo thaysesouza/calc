6f7ecc832dde6bbaa56db3b472583973c7eda43b
Thayse Souza <thayseenf14@gmail.com>
2020-10-31 12:09:15 -0300

    Primeiro exercicio com If Ternario

--- ternário.rb
+++ ternário.rb
@@ -2,7 +2,7 @@
 idade = 0 
 
 print "Olá! Descubra se uma pessoa é adolescente.
-Informe a idade desta pessa: "
+Informe a idade desta pessoa: "
    idade = gets.chomp.to_i
 
 puts (11...18).include?(idade) ? "Essa pessoa é adolescente!" : "Essa pessoa não é adolescente!"
