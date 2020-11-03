0fa348665ceeabfcdc28cea8f1606eccec134883
Thayse Souza <thayseenf14@gmail.com>
2020-11-03 07:45:50 -0300

    Exercicio com for - Tabuada

--- tabuada.rb
+++ tabuada.rb
@@ -1,11 +1,11 @@
 # tabuada
 numero = 0
 
-puts "Digite um número: "
+puts "Digite um número real: "
     numero = gets.chomp.to_f
 
 for i in 1..10
-    puts "#{numero} * #{i} = #{numero * i}\n"
-    puts "#{numero} / #{i} = #{(numero / i).round(2)}\n"
-    puts "#{numero} ** #{i} = #{numero ** i}\n"
+    puts "\n#{numero} * #{i} = #{(numero * i).round(2)}\n"
+    puts "\n#{numero} / #{i} = #{(numero / i).round(2)}\n"
+    puts "\n#{numero} ** #{i} = #{(numero ** i).round(2)}\n"
 end
